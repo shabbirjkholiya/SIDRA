e02bd8681f601cf6559fb2067de0efe345b27243
shabbirjkholiya <shabbirjkholiya@gmail.com>
2025-10-06 20:16:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e){
+  e.preventDefault();
+  alert('Thank you for contacting SIDRA Sofa & Deep Cleaning! We will reach out soon.');
+  this.reset();
+});
